410ad7b350942a70c87326c61261b827d1f5ed5d
shravanj <shravanj@COL011022605.infics.com>
2020-04-22 12:27:44 +0530

    Class and Object

---
+++ JavaTrainingSession/src/OOPConceptsPart1/CarClass.java
@@ -0,0 +1,32 @@
+package OOPConceptsPart1;
+
+public class CarClass {
+	
+	//Class variables
+	
+	int mod;
+	int wheel;
+	
+	public static void main(String[] args) {
+		
+	//new CarClass --- this is the object of the Car class	
+		CarClass obj1 = new CarClass();
+		CarClass obj2 = new CarClass();
+		CarClass obj3 = new CarClass();
+		
+		obj1.mod = 2020;
+		obj1.wheel =4;
+		
+		obj2.mod = 2019;
+		obj2.wheel = 4;
+		
+		obj3.mod = 2018;
+		obj3.wheel = 4;
+		
+		System.out.println(obj1.mod);
+		System.out.println(obj1.wheel);
+	
+
+	}
+
+}
